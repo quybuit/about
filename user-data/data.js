21b6c243e168a81445a4e392eca2377867aaee89
quybuit <quybuit@gmail.com>
2025-04-04 19:52:25 +0700

    add sample content

--- user-data/data.js
+++ user-data/data.js
@@ -1,7 +1,8 @@
 export const bio = [
-  "Hello, I'm Vinay Somawat!",
-  "<strong>Frontend Software Engineer</strong> with 4+ years of experience building scalable, user-focused web applications.",
-  "Skilled in Angular, TypeScript, and performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
+  "Hello, I'm Quy Bui!",
+  "I possess a robust background in crafting enterprise-level web applications utilizing C# and the .NET Framework accumulated focus on the MicroServices architecture.",
+  "My expertise lies in developing high-performance, scalable applications with a strong emphasis on code quality and maintainability.",
+  "Skilled in TypeScript, ReaactJS, Vuejs... performance optimization, I aim to leverage my expertise in delivering innovative solutions that drive measurable business outcomes.",
   "Passionate about mentoring, teamwork, and continuous improvement in engineering processes.",
 ];
 
